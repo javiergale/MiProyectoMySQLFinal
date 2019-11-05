7e46ba28c79c6ca013c40cd61db3af4e740d16ad
javiergale <javiergaleanomendoza1999@gmail.com>
2019-11-05 16:09:28 -0600

    App Final

--- app/src/main/java/com/example/javier/Config.java
+++ app/src/main/java/com/example/javier/Config.java
@@ -2,7 +2,7 @@ package com.example.javier;
 
 public class Config {
 
-    /*
+
     //URL PARA ACCESO REMOTA A BASE DE DATOS MYSQL DEL SERVIDOR DEL PROF. GÁMEZ EN LA NUBE.
     public static final String urlGuardar = "http://mjgl.com.sv/mysqlcrud/guardar.php";                               //Guardar datos DB.
     public static final String urlEliminar = "http://mjgl.com.sv/mysqlcrud/eliminar.php";                             //Eliminar datos DB.
@@ -14,9 +14,9 @@ public class Config {
     public static final String urlConsultaCodigo = "http://mjgl.com.sv/mysqlcrud/buscarArticulosCodigo.php";            //Busquedas por código.
     public static final String urlConsultaDescripcion = "http://mjgl.com.sv/mysqlcrud/buscarArticulosDescripcion.php";  //Busquedas por descripción.
     public static final String urlConsultaAllArticulos = "http://mjgl.com.sv/mysqlcrud/buscarArticulos.php";            //Devuelve todos los registros de la tabla MySQL.
-    */
 
-    //URL PARA ACCESO A BASE DE DATOS LOCAL MYSQL.
+
+   /* //URL PARA ACCESO A BASE DE DATOS LOCAL MYSQL.
     //public static final String urlGuardar = "http://192.168.43.46/mysqlcrud/guardar.php";                               //Guardar datos DB.
     public static final String urlGuardar = "http://192.168.187.104/crudsis22b/guardar.php";                               //Guardar datos DB.
 
@@ -30,5 +30,5 @@ public class Config {
     public static final String urlConsultaDescripcion = "http://192.168.187.104/crudsis22b/buscarArticulosDescripcion.php";  //Busquedas por descripción.
     public static final String urlConsultaAllArticulos = "http://192.168.187.104/crudsis22b/buscarArticulos.php";            //Devuelve todos los registros de la tabla MySQL.
 
-
+*/
 }
